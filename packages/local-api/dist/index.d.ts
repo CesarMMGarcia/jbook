5dda4d920dd8eaa1aeee27b64a4376cb5155c720
imhyperion <imhyperion@users.noreply.github.com>
2021-02-18 14:38:04 -0800

    local-api not working

---
+++ packages/local-api/dist/index.d.ts
@@ -0,0 +1,2 @@
+export declare const serve: (port: number, filename: string, dir: string, useProxy: boolean) => Promise<void>;
+export declare const serve: (port: number, filename: string, dir: string, useProxy: boolean) => Promise<void>;
